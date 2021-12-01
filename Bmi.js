d29554115bad22e650cb92be58a9df5aacbec780
fatima <fatimamahlaba@gmail.com>
2021-12-01 16:08:27 +0200

    last commit

--- Bmi.js
+++ Bmi.js
@@ -1,8 +1,18 @@
 function BMI() {
-  var h=document.getElementById('h').value;
-  var w=document.getElementById('w').value;
-  var bmi=w/(h/100*h/100);
-  var bmio=(bmi.toFixed(2));
+  let h=document.getElementById('h').value;
+  let w=document.getElementById('w').value;
+  let bmi=w/(h/100*h/100);
+  let bmio=(bmi.toFixed(2));
 
-  document.getElementById("result").innerHTML="Your BMI is " + bmio;
+  document.getElementById("display").innerHTML="Your BMI is " + bmio;
 }
+
+
+
+
+
+
+
+
+
+
--- Temp.js
+++ Temp.js
@@ -1,3 +1,3 @@
 function tempconv(){ let itemp= parseInt(document.getElementById('temp').value) ; 
 let newtemp= (itemp * 9/5) + 32;     
-     alert('Your new temperature is'+ " "+newtemp)};
+     alert('Your new temperature in Fahrenheit (°F):'+ " "+newtemp)};
